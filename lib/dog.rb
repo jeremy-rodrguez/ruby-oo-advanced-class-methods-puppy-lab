be8c30ff104cbd9209d8f49dc1df36d0d10c70cb
Jeremy <jeremy.rodrguez@yahoo.com>
2020-11-19 19:10:14 -0800

    commit tree 4cf35ace1c1b7d06fe37a07fabe08153f426971c

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+
+class Dog
+    attr_accessor :name
+    
+   @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+        # @@all << self We extracted this and created it's own method
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        # @@all.map(&:name) is an optional method according to notes
+        puts @@all.map { |dog| dog.name}
+    end
+
+    def save
+        @@all << self
+    end
+end
